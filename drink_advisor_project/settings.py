8553ced6ace45c47e2b45fa78d7ae3751cf6278f
Maxwell McLaughlan <2137949m@student.gla.ac.uk>
2018-03-14 13:12:00 +0000

    favicon

--- drink_advisor_project/settings.py
+++ drink_advisor_project/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -119,4 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATICFILES_DIRS = [STATIC_DIR, ]
+
 STATIC_URL = '/static/'
+
+# Media variables
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
