8d8edd28c7a687d743f76294c78927bb0960631b
Huriyah Hussain <2241433h@student.gla.ac.uk>
2018-03-16 09:46:26 +0000

    Log in page

--- drinkadvisor/views.py
+++ drinkadvisor/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from drinkadvisor.forms import UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login
+from django.core.urlresolvers import reverse
+
 
 # Create your views here.
 
@@ -74,8 +77,32 @@ def register(request):
                   'drinkadvisor/register.html',
                   {'user_form': user_form,
                    'profile_form': profile_form,
-                   'registered': registered})
+                   'registered': registered})
 
+
+def user_login(request):
+    if request.method == 'POST':
+        
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse("Your Drink Advisor account is disabled.")
+        else:
+            print("Invalid login details: {0}, {1}".format(username, password))
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'drinkadvisor/login.html', {})
+        
+            
+                
+                
             
     
         
