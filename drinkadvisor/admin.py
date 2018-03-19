67c17459f58b99b5f44440afbb865efee4db0308
Ubaidullah Khan <2271135k@student.gla.ac.uk>
2018-03-19 19:47:53 +0000

    created the models for drinks and feedback

--- drinkadvisor/admin.py
+++ drinkadvisor/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from drinkadvisor.models import UserProfile
+from drinkadvisor.models import UserProfile, Drink, Feedback
 
 # Register your models here.
 
 
 admin.site.register(UserProfile)
+admin.site.register(Drink)
+admin.site.register(Feedback)
--- drinkadvisor/models.py
+++ drinkadvisor/models.py
@@ -1,14 +1,45 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
+import datetime
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User)
+
     website = models.URLField(blank=True)
-    picture = models.ImageField(upload_to='profile_images', blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
 
     def __str__(self):
-        return self.user.username    
+        return self.user.username
     
     
+    
+
+class Drink(models.Model):
+    name = models.CharField(max_length=50)
+    calories = models.IntegerField(default = 0)
+    caffeine = models.IntegerField(default=0)
+    isEnergyDrink = models.BooleanField(default = False)
+    isSugarFree = models.BooleanField(default = False)
+    date_uploaded = models.DateField(("Date"), default=datetime.date.today)
+
+    class Meta:
+        verbose_name_plural = 'Drinks'
+
+    def __str__(self):
+        return self.name
+
+class Feedback(models.Model):
+    drink = models.ForeignKey(Drink)
+    content = models.CharField(max_length = 400)
+    rate = models.IntegerField(null = True)
+    date_uploaded = models.DateField(("Date"), default=datetime.date.today)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Feedbacks'
+
+    def __str__(self):
+        return self.content
