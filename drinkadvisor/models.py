e68ae17b4f4b8e49b520601e3076a2dbc9ca7f8f
Huriyah Hussain <2241433h@student.gla.ac.uk>
2018-03-20 10:17:56 +0000

    Picture shown on profile

--- drinkadvisor/models.py
+++ drinkadvisor/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
-
+from django.db.models.signals import post_save
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
@@ -12,6 +12,12 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+   # def create_profile(sender, **kwargs):
+      #  if kwargs['created']:
+       #     user_profile = UserProfile.objects.create(user=kwargs['instance'])
+
+   # post_save.connect(create_profile, sender=User)
     
     
     
